83f73c495fbc294bb25562189c6f3ffd1f73ed78
Carter Dodd <carter.dodd@gmail.com>
2017-08-13 13:57:59 -0500

    graphql pattern

---
+++ graphql.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const graphQLHTTP = require("express-graphql");
+const DataLoader = require("dataloader");
+const Promise = require("bluebird");
+
+const schema = require("./graphqlschema.js");
+
+
+const app = express();
+
+const persons = [{
+    firstname: "firstname1",
+    lastname: "lastname1",
+    email: "firstname1.lastname1@website.com",
+    username: "username1",
+    friends: [1,2]
+  },
+  {
+    firstname: "firstname2",
+    lastname: "lastname2",
+    email: "firstname2.lastname2@website.com",
+    username: "username2",
+    friends: [0,2]
+  },
+  {
+    firstname: "firstname3",
+    lastname: "lastname3",
+    email: "firstname3.lastname3@website.com",
+    username: "username3",
+    friends: [0,1]
+}];
+
+app.use(graphQLHTTP(req => {
+
+  const personLoader = new DataLoader(
+    keys => Promise.all(keys.map((id) => persons[id]))
+  );
+
+  const loaders = {
+    person: personLoader
+  };
+
+  return {
+    context: {loaders},
+    schema,
+    graphiql: true
+  };
+}));
+
+app.listen(5000);
---
+++ graphqlschema.js
@@ -0,0 +1,38 @@
+const graphql = require("graphql");
+
+const PersonType = new graphql.GraphQLObjectType({
+  name: "Person",
+  description: "...",
+  fields: () => ({
+    firstname: {
+      type: graphql.GraphQLString
+    },
+    lastname: {
+      type: graphql.GraphQLString
+    },
+    email: {type: graphql.GraphQLString},
+    username: {type: graphql.GraphQLString},
+    friends: {
+      type: new graphql.GraphQLList(PersonType),
+      resolve: (person, args, {loaders}) => loaders.person.loadMany(person.friends)
+    }
+  })
+});
+
+const QueryType = new graphql.GraphQLObjectType({
+  name: "Query",
+  description: "...",
+  fields: () => ({
+    person: {
+      type: PersonType,
+      args: {
+        id: {type: graphql.GraphQLInt}
+      },
+      resolve: (root, args, {loaders}) => loaders.person.load(args.id)
+    }
+  })
+});
+
+module.exports = new graphql.GraphQLSchema({
+  query: QueryType
+});
